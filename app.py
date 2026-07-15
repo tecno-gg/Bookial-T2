17a3b96d73f0449c3a14d80c31859ecb9d80424c
ebracamontecunes-cmd <e.bracamontecunes@liceorbl.cl>
2026-07-15 07:24:12 -0300

    nada

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 def index():
 	return render_template('public/index.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['FORM_REGISTER'])
 def register():
-	request.form['first_name']
-	request.form['first_surname']
-	request.form['email']
-	request.form['password']
+	session['name01'] = 	 request.form['name01']
+	session['name02'] = 	 request.form['name02']
+	session['surname01'] = request.form['surname01']
+	session['surname02'] = request.form['surname02']
+	session['email'] =		 request.form['email']
+	session['password'] =	 request.form['password']
 	return render_template('public/register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['FORM_LOGIN'])
 def login():
 	return render_template('public/login.html')
